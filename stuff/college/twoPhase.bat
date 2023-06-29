f9c127a713d9868d179c9792302e2f108dd7738f
Brett Wilson <brett.david.wilson.bdw@outlook.com>
2023-06-29 11:21:39 +0200

    Add files via upload

---
+++ stuff/college/twoPhase.bat
@@ -0,0 +1,16 @@
+@echo off
+@REM echo calc --var 2 --const 5 --round false
+set /p var="num of obj func vars: "
+set /p const="num of constraints: "
+set roundVal="false"
+set /p round="round(y/n): "
+if "%round%" == "y" (
+    set /p roundVal="num of dec places: "
+)
+if %roundVal% NEQ "false" (
+    java -jar simplex-calc.jar calc --var %var% --const %const% --round %roundVal%
+)
+if "%round%" == "n" (
+    java -jar simplex-calc.jar calc --var %var% --const %const% --round false
+)
+pause
